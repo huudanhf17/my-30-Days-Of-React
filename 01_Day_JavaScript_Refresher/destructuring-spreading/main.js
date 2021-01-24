3115de68145e841239caaf82a81c8bb953f1b3ba
Danh <adc9gs@gmail.com>
2021-01-24 11:29:18 +0700

    commit day1 v6

---
+++ 01_Day_JavaScript_Refresher/destructuring-spreading/main.js
@@ -0,0 +1,4 @@
+/*
+Create a function called getPersonInfo. The getPersonInfo function takes an object parameter. The structure of the object and the output of the function is given below. Try to use both a regular way and destructuring and compare the cleanness of the code. If you want to compare your solution with my solution, check this link.
+*/
+
--- 01_Day_JavaScript_Refresher/functions/exercise3/main.js
+++ 01_Day_JavaScript_Refresher/functions/exercise3/main.js
@@ -78,3 +78,12 @@ function isEmpty(parameter) {
     console.log("Not");
   }
 }
+
+// 6: Write a function called average, it takes an array parameter and returns the average of the items. Check if all the array items are number types. If not give return reasonable feedback.
+function average (array) {
+  if (array.every(value=>typeof value === 'number')){
+    return array.reduce((acc,curValue)=>acc+curValue,0)/array.length
+  } else {
+    return "Some parameter isn't number";
+  };
+};
