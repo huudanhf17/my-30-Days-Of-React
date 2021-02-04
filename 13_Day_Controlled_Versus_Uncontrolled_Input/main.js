81efde79773ef8d09ee7820fceda73d05b49d14a
Danh Tran Huu <thuudanh@tma.com.vn>
2021-02-04 17:51:59 +0700

    commit day 13 -- express v1

---
+++ 13_Day_Controlled_Versus_Uncontrolled_Input/main.js
@@ -0,0 +1,31 @@
+/* ------ Exercises ------- */
+
+// 1: What is a controlled input?
+// Controlled input typically maintain its own state and update it based on user input. In React, mutable state is typically kept in the state property of components, and only updated with setState().
+
+// 2: What is an uncontrolled input
+// The form data is handled by the DOM itself.
+
+// 3: How do you get a content of a certain HTML element in React ?
+// By state
+
+// 4: Why it is not a good idea to touch the DOM directly in React ?
+// Because React has its own way of handling the DOM manipulation.
+
+// 5: What is most frequently used in React ? Controlled or Uncontrolled input.
+// Controlled input.
+
+// 6: What do you need to write uncontrolled input?
+// ref, type
+
+// 7: Does state require to write uncontrolled input?
+// No
+
+// 8: When do you use uncontrolled input?
+// uncontrolled components should use for simple, functional systems. Then it will take advantage of its simple and fast installation. Furthermore user interaction will be better because the web page won't have to re-render every time the input value changes.
+
+// 9: When do you use controlled input?
+// When need to handle data by state
+
+// 10: Do you use a controlled or uncontrolled input to validate form input fields?
+// Controlled
