0c1648b07cae6abc651c12d0d86ad06ee7895944
Danh Tran Huu <thuudanh@tma.com.vn>
2021-04-22 17:58:19 +0700

    day35 v2

--- 35_graphql/resolver/resolver.js
+++ 35_graphql/resolver/resolver.js
@@ -1,17 +1,26 @@
+const { books, authors } = require("../data/static");
+
 const resolves = {
   Query: {
-    books: () => [
-      {
-        id: 1,
-        name: "De Men Phieu Luu Ky",
-        genre: "Adventure",
-      },
-      {
-        id: 2,
-        name: "Làm giau khong kho",
-        genre: "Education",
-      },
-    ],
+    books: () => books,
+    book: (parent, args) => books.find((book) => book.id == args.id),
+    authors: () => authors,
+    author: (parent, args) => authors.find((author) => author.id == args.id),
+  },
+
+  Book: {
+    author: (parent, args) =>
+      authors.find((author) => author.id == parent.authorId),
+  },
+
+  Author: {
+    books: (parent, args) =>
+      books.filter((book) => book.authorId === parent.id),
+  },
+
+  Mutation: {
+    createAuthor: (parent, args) => args,
+    createBook: (parent, args) => args,
   },
 };
 
